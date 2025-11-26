b824079930ed553d1110e72204b424d365116b9a
Mr_Fliver <mrfliver255@gmail.com>
2025-11-26 14:55:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-BOT_TOKEN = "Ваш токен"
-WEATHER_API_KEY = "e4b93ed98df342f2904201539252510"
-ADMIN_IDS = [7638967663]
-GROUP_LINK = "Ваша ссылка на канал"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "7638967663").split(",")]
+GROUP_LINK = os.getenv("GROUP_LINK", "Ваша ссылка на канал")
